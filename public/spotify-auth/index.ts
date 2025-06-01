7163467b380bbdc6c503cb73b9f9958531668022
Alimnfl <mnaufallim234@gmail.com>
2025-06-02 06:39:49 +0700

    Add image for spotify auth

---
+++ public/spotify-auth/index.ts
@@ -0,0 +1,6 @@
+import SpotifyIcon from "./spotify-white-icon.png";
+import GoogleIcon from "./google.webp";
+import FacebookIcon from "./facebook.png";
+import MacOsIcon from "./mac-os.png";
+
+export { SpotifyIcon, FacebookIcon, GoogleIcon, MacOsIcon };
